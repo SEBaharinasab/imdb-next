4d6e36571d6327ffac1713cbb3cfbdc862343980
SEBaharinasab <36398369+SEBaharinasab@users.noreply.github.com>
2022-01-14 22:43:58 +0330

    bug fix

--- pages/movies/index.jsx
+++ pages/movies/index.jsx
@@ -3,7 +3,7 @@ import MoviesList from "../../components/MoviesList";
 import { useState } from "react";
 
 function index() {
-   const [movies, setMovies] = useState();
+   const [movies, setMovies] = useState({});
    return (
       <div className='flex flex-col gap-2 lg:flex-row lg:p-8 lg:gap-8'>
          <div className='w-full lg:w-1/3'>
